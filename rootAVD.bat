36d16cf12e7f29fc014dee149f0b99b53c4bec04
NewBit <37043777+newbit1@users.noreply.github.com>
2021-03-03 10:56:44 +0100

    updated Magisk App v22.0 and Android 11 (S) Support

--- rootAVD.bat
+++ rootAVD.bat
@@ -7,8 +7,12 @@ REM # modded by NewBit XDA for Android Studio AVD
 REM # Successfully tested on Android API:
 REM # [Dec. 2019] - 29 Google Apis Play Store x86_64 Production Build
 REM # [Jan. 2021] - 30 Google Apis Play Store x86_64 Production Build
+REM # [Mar. 2021] - 30 Android (S) Google Apis Play Store x86_64 Production Build
 REM #
 REM ##########################################################################################
+REM rootAVD.bat %LOCALAPPDATA%\Android\Sdk\system-images\android-S\google_apis_playstore\x86_64\ramdisk.img
+REM rootAVD.bat %LOCALAPPDATA%\Android\Sdk\system-images\android-30\google_apis_playstore\x86_64\ramdisk.img
+REM rootAVD.bat %LOCALAPPDATA%\Android\Sdk\system-images\android-29\google_apis_playstore\x86_64\ramdisk.img
 
 if "%1" == "" (
     echo "rootAVD needs a path with file to an AVD ramdisk"
@@ -21,8 +25,6 @@ set RAMDISKFILE=%1
 set BACKUPFILE=%RAMDISKFILE%.bak
 set ROOTAVD="%cd%"
 set MAGISKZIP=%ROOTAVD%\Magisk.zip
-set BUSYBOXINSTLR=%ROOTAVD%\update-binary
-
 
 CD %RAMDISKFILE%
 IF "%ERRORLEVEL%"=="0" (
@@ -70,9 +72,6 @@ adb shell mkdir %ADBBASEDIR%
 echo "[-] Copy Magisk installer Zip"
 adb push %MAGISKZIP% %ADBBASEDIR%
 
-echo "[*] Copy Busybox installer"
-adb push %BUSYBOXINSTLR% %ADBBASEDIR%
-
 echo "[*] Copy the original AVD ramdisk.img into Magisk DIR"
 adb push %RAMDISKFILE% %ADBBASEDIR%
 
@@ -85,9 +84,12 @@ adb -e shell "dos2unix %ADBBASEDIR%/rootAVD.sh"
 echo "[-] run the actually Boot/Ramdisk/Kernel Image Patch Script"
 echo "[*] from Magisk by topjohnwu and modded by NewBit XDA"
 adb shell sh %ADBBASEDIR%/rootAVD.sh "ranchu"
-echo "[-] After the ramdisk.img file is patched and back gz'ed,"
+echo "[-] After the ramdisk.img file is patched and compressed,"
 echo "[*] pull it back in the Magisk DIR"
 adb pull %ADBBASEDIR%/ramdiskpatched4AVD.img
+
+echo "[-] pull Magisk.apk to Apps\"
+adb pull %ADBBASEDIR%/Magisk.apk Apps\
 
 echo "[-] Clean up the ADB working space"
 adb shell rm -rf %ADBBASEDIR%
